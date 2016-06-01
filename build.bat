76e1ed6c00b66d82772ef76bc5b3c6639bfed9ca
Dmitry Grigoryev <dmitriy57@gmail.com>
2016-06-01 13:10:23 +0300

    Build scripts now build both platform versions of enscheme

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 mkdir build
 del build\enscheme.exe
+del build\enscheme
 fasm-win\fasm src\enscheme-win32.asm build\enscheme.exe
+fasm-win\fasm src\enscheme-linux32.asm build\enscheme
 
--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 mkdir build
 rm build/enscheme
+rm build/enscheme.exe
 fasm-linux/fasm src/enscheme-linux32.asm build/enscheme
+fasm-linux/fasm src/enscheme-win32.asm build/enscheme.exe
 
