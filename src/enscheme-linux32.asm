47ba4181ca51c7e97876e4515f000ef72854f22c
Dmitry Grigoryev <dmitriy57@gmail.com>
2016-07-05 10:15:10 +0300

    Refactored core code into one file

--- src/enscheme-linux32.asm
+++ src/enscheme-linux32.asm
@@ -4,16 +4,29 @@
 ; This file is part of enscheme project.
 ; Copyright (c) 2015-2016, Dmitry Grigoryev
 
-include 'macros.inc'
-include 'proc.inc'
+macro syscall_before
+{
+        push ebx
+        push esi
+        push edi
+        push ebp
+}
 
-include 'unicode.inc'
-include 'core.inc'
-include 'runner.inc'
-include 'memory.inc'
+macro syscall_after
+{
+        pop ebp
+        pop edi
+        pop esi
+        pop ebx
+}
 
+macro PLATFORM_HEADER
+{
 format ELF executable 3
+}
 
+macro PLATFORM_CODE
+{
 entry platform_start
 
 segment executable
@@ -21,7 +34,7 @@ segment executable
 platform_start:
         ; Clear direction flag for calling conventions
         cld
-        
+
         pop [argc]
         mov [argv], esp
 
@@ -33,33 +46,219 @@ platform_start:
         mov eax, 1
         int 0x80
 
-include 'system-linux32.inc'
-unicode_code
-core_code
-runner_code
-memory_code
+; Initializes platform-specific linux variables
+linux32_init:
+        push ebp
+        mov ebp, esp
+        push esi
 
+        ccall linux_syscall, 45, 0
+        mov [cur_brk], eax
+
+        mov eax, [argc]
+        shl eax, 4
+        ccall brk_alloc, eax
+        mov [argl], eax
+
+        xor esi, esi
+    .next_arg:
+        mov edx, [argv]
+        ccall length, [edx+esi*4]
+        mov edx, [argl]
+        mov [edx+esi*4], eax
+
+        inc esi
+        cmp esi, [argc]
+        jb .next_arg
+
+        pop esi
+        mov esp, ebp
+        pop ebp
+        ret
+
+; Terminates program
+; In: 1) exit code
+exit:
+        mov eax, 1
+        mov ebx, [esp+4]
+        int 0x80
+
+; Allocates memory using brk
+; In: 1) Size of memory
+; Out: Address of allocated memory block
+;      Returns NULL on error
+brk_alloc:
+malloc:
+        mov eax, [cur_brk]
+        add eax, [esp+4]
+        ccall linux_syscall, 45, eax
+        mov edx, [cur_brk]
+        cmp edx, eax
+        je .error
+        mov [cur_brk], eax
+        mov eax, edx
+        ret
+    .error:
+        xor eax, eax
+        ret
+
+; Writes a string to standard output
+; In: 1) Address of string
+;     2) Length of string
+write_string:
+        push ebp
+        mov ebp, esp
+        ccall linux_syscall, 4, 1, [ebp+8], [ebp+12]
+        mov esp, ebp
+        pop ebp
+        ret
+
+; Writes new line to standard output
+write_newline:
+        ccall linux_syscall, 4, 1, newline_str, newline_len
+        ret
+
+; mmap2 syscall
+; In: 1) address for the new mapping
+;     2) length of the mapping
+;     3) desired memory protection
+;     4) flags
+;     5) file descriptor
+;     6) offset into file (specified in 4096-bytes pages)
+; Out: On success return address of the mapped area
+;      On error returns -ERRORCODE, which has range -133..-1
+mmap2:
+        push ebp
+        mov ebp, esp
+        ccall linux_syscall, 192, [ebp+8], [ebp+12], [ebp+16],\
+                [ebp+20], [ebp+24], [ebp+28]
+        mov esp, ebp
+        pop ebp
+        ret
+
+; Linux syscall
+; In: 1) Syscall number
+; 2-7) 6 syscall parameters
+; Out: syscall result
+;      On error returns -ERRORCODE, which has range -133..-1
+linux_syscall:
+        push ebp
+        mov ebp, esp
+        syscall_before
+        mov eax, [ebp+8]
+        mov ebx, [ebp+12]
+        mov ecx, [ebp+16]
+        mov edx, [ebp+20]
+        mov esi, [ebp+24]
+        mov edi, [ebp+28]
+        mov ebp, [ebp+32]
+        int 0x80
+        syscall_after
+        mov esp, ebp
+        pop ebp
+        ret
+
+stat64.size equ 0x2c
+
+; Reads a file into memory
+; In: 1) File path (null-terminated string)
+; Out: EAX - memory address of file data (NULL in case of error)
+;      EDX - length of file data
+read_file:
+        push ebp
+        mov ebp, esp
+        sub esp, 0x6c
+        .handle equ ebp-0x04
+        .buffer equ ebp-0x08
+        .size equ ebp-0x0c
+        .statbuf equ ebp-0x6c
+
+        ; Open file
+        ccall linux_syscall, 5, [ebp+0x08], 0, 0
+        ; Test if result < 0
+        test eax, eax
+        js .error
+        mov [.handle], eax
+
+        ; stat64 syscall
+        lea eax, [.statbuf]
+        ccall linux_syscall, 197, [.handle], eax
+        test eax, eax
+        jnz .errorclose
+
+        ; Check that file size fits into 32 bits
+        mov eax, [.statbuf+stat64.size+4]
+        test eax, eax
+        jnz .errorclose
+
+        ; Check that file size fits into 31 bits
+        mov eax, [.statbuf+stat64.size]
+        add eax, 4
+        test eax, eax
+        js .errorclose
+
+        mov [.size], eax
+
+        ; Allocate memory
+        ccall linux_syscall, 192, 0, [.size], 0x3, 0x0022, -1, 0
+        cmp eax, 0xffffe000
+        jae .errorclose
+        mov [.buffer], eax
+        mov edx, [.size]
+        mov [eax], edx
+
+        ; Read contents
+        add eax, 4
+        ccall linux_syscall, 3, [.handle], eax, [.statbuf+stat64.size]
+        cmp eax, [.statbuf+stat64.size]
+        jne .errorfree
+
+        ccall linux_syscall, 6, [.handle]
+        mov eax, [.buffer]
+        add eax, 4
+        mov edx, [.statbuf+stat64.size]
+        jmp .ret
+
+    .errorfree:
+        ccall linux_syscall, 91, [.buffer], [.size]
+    .errorclose:
+        ccall linux_syscall, 6, [.handle]
+    .error:
+        xor eax, eax
+    .ret:
+        mov esp, ebp
+        pop ebp
+        ret
+
+
+; Clears memory allocated for file
+; In: 1) Address of memory allocated for file
+finish_file:
+        mov eax, [esp+4]
+        sub eax, 4
+        mov edx, [eax]
+        ccall linux_syscall, 91, eax, edx
+        ret
+}
+
+macro PLATFORM_RDATA
+{
 segment readable
-def_string newline, 10
-unicode_rodata
-core_rodata
-runner_rodata
-memory_rodata
+}
 
+macro PLATFORM_DATA
+{
 segment readable writeable
-unicode_rwdata
-core_rwdata
-runner_rwdata
-memory_rwdata
+}
 
+macro PLATFORM_BSS
+{
 segment readable writeable
         argc            rd  1
         argv            rd  1
         argl            rd  1
         cur_brk         rd  1
+}
 
-unicode_bss
-core_bss
-runner_bss
-memory_bss
+include 'enscheme.inc'
 
--- src/enscheme-win32.asm
+++ src/enscheme-win32.asm
@@ -4,16 +4,13 @@
 ; This file is part of enscheme project.
 ; Copyright (c) 2015-2016, Dmitry Grigoryev
 
-include 'macros.inc'
-include 'proc.inc'
-
-include 'unicode.inc'
-include 'core.inc'
-include 'runner.inc'
-include 'memory.inc'
-
+macro PLATFORM_HEADER
+{
 format PE console
+}
 
+macro PLATFORM_CODE
+{
 section '.code' code executable readable
 
 entry platform_start
@@ -26,28 +23,472 @@ platform_start:
 
         ccall main, [argc], [argv], [argl]
 
-        ; Terminate program
         stdcall [ExitProcess], eax
 
-include 'system-win32.inc'
-unicode_code
-core_code
-runner_code
-memory_code
 
-section '.rodata' data readable
-def_string newline, 10
-unicode_rodata
-core_rodata
-runner_rodata
-memory_rodata
+; Initializes platform-specific win32 variables
+win32_init:
+        push ebp
+        mov ebp, esp
+        sub esp, 16
+        push esi
+        push edi
 
+        ; Retrieve standard output handle
+        stdcall [GetStdHandle], -11
+        mov [hStdout], eax      ; Store it in hStdout
+
+        ; Retrieve standard input handle
+        stdcall [GetStdHandle], -10
+        mov [hStdin], eax       ; Store it in hStdin
+
+        ; Get and parse command line
+        stdcall [GetCommandLineW]
+        stdcall [CommandLineToArgvW], eax, argc
+        mov [argv16], eax       ; arguments list goes to argv
+
+        ; Initialize heap
+        stdcall [GetProcessHeap]
+        mov [win32_heap], eax
+
+        ; Allocate space for argv
+        mov esi, [argc]
+        shl esi, 2
+        stdcall [HeapAlloc], [win32_heap], 0, esi
+        mov [argv], eax
+        stdcall [HeapAlloc], [win32_heap], 0, esi
+        mov [argl], eax
+
+        xor esi, esi
+
+    .next_arg:
+        ; [ebp-4] - number of 16-bit words in UTF-16 argument
+        ; [ebp-8] - address of UTF-16 argument
+        ; [ebp-12] - length of UTF-8 argumnet
+        ; [ebp-16] - address of UTF-8 argument
+        ;; Get next argument and its length
+        mov eax, [argv16]       ; Load address of argument list into EAX
+        mov edi, [eax+4*esi]    ; Load address of next argument into EDI
+        mov [ebp-8], edi        ; Address of argument
+        ccall length16, edi     ; Length of argument into EAX
+        mov [ebp-4], eax        ; Length of argument
+
+        ;; Get length of argument converted to UTF-8
+        ccall utf16_to_utf8_length, [ebp-8], [ebp-4]
+                                ; UTF-8 length is in EAX
+        mov [ebp-12], eax       ; Length of UTF-8 result
+
+        ;; Convert argument to UTF-8
+        ; Allocate buffer for converting
+        mov eax, [ebp-12]
+        inc eax                 ; For extra terminating null byte
+        stdcall [HeapAlloc], [win32_heap], 0, eax
+        mov [ebp-16], eax       ; Address of buffer for result
+
+        ; Convert argument
+        mov eax, [ebp-12]
+        inc eax                 ; For extra terminating null byte
+        ccall utf16_to_utf8, [ebp-8], [ebp-4], [ebp-16], eax
+
+        ; Store address of argument in argv
+        mov eax, [argv]
+        mov edx, [ebp-16]
+        mov [eax+4*esi], edx
+        ; Store length of argument in argv
+        mov eax, [argl]
+        mov edx, [ebp-12]
+        mov [eax+4*esi], edx
+
+        inc esi
+        cmp esi, [argc]
+        jb .next_arg
+
+        pop edi
+        pop esi
+        mov esp, ebp
+        pop ebp
+        ret
+
+; Terminate program
+; In: 1) exit code
+exit:
+        mov eax, [esp+4]
+        stdcall [ExitProcess], eax
+
+
+; Writes a string to standard output
+; In: 1) Address of string
+;     2) Length of string
+write_string:
+        mov eax, [esp+4]
+        mov edx, [esp+8]
+        stdcall [WriteFile], [hStdout], eax, edx, bytes_written, 0
+        ret
+
+; Writes new line to standard output
+write_newline:
+        stdcall [WriteFile], [hStdout], newline_str, newline_len, \
+            bytes_written, 0
+        ret
+
+; Reads a file into memory
+; In: 1) File path (null-terminated string)
+; Out: EAX - memory address of file data (NULL in case of error)
+;      EDX - length of file data
+read_file:
+        push ebp
+        mov ebp, esp
+        sub esp, 24
+
+        .handle equ ebp-0x04
+        .buffer equ ebp-0x08
+        .size_high equ ebp-0x0c
+        .size_low equ ebp-0x10
+        .bytes_written equ ebp-0x14
+        .result equ ebp-0x1c
+        ; Locals:
+        ; [EBP-4] File handle
+        ; [EBP-8] Allocated memory address
+        ; [EBP-12] High doubleword of file size
+        ; [EBP-16] Low doubleword of file size
+        ; [EBP-20] Number of bytes read
+        ; [EBP-24] Result of ReadFile
+
+        stdcall [CreateFileA], [ebp+8], 0x80000000, 1, 0, 3, 0, 0
+        mov [.handle], eax
+        cmp eax, -1
+        je .error
+
+        lea edx, [.size_low]
+        stdcall [GetFileSizeEx], eax, edx
+        test eax, eax
+        jz .error
+
+        cmp dword [.size_high], 0
+        jnz .error
+
+        stdcall [HeapAlloc], [win32_heap], 0, [ebp-16]
+        mov [.buffer], eax
+        test eax, eax
+        jz .error
+
+        lea edx, [.bytes_written]
+        stdcall [ReadFile], [.handle], [.buffer], [.size_low], edx, 0
+        mov [.result], eax
+        stdcall [CloseHandle], [.handle]
+
+        cmp dword [.result], 0
+        je .error
+
+        mov eax, [.buffer]
+        mov edx, [.size_low]
+        jmp .ret
+
+    .error:
+        mov eax, 0
+
+    .ret:
+        mov esp, ebp
+        pop ebp
+        ret
+
+; Clears memory allocated for file
+; In: 1) Address of memory allocated for file
+finish_file:
+        mov eax, [esp+4]
+        stdcall [HeapFree], [win32_heap], 0, eax
+        ret
+
+; Allocates memory
+; In: 1) Size of memory
+; Out: Address of allocated memory block
+;       NULL if failed
+malloc:
+        mov eax, [esp+4]
+        stdcall [HeapAlloc], [win32_heap], 0, eax
+                ret
+
+; Calculates number of 16-bit characters in null-terminated UTF-16 string
+; In: 1) address of null-terminated UTF-16 string
+; Out: length of the string
+; Preserves all other registers
+length16:
+        mov eax, [esp+4]
+        mov ecx, 0
+        jmp .start
+    .next:
+        inc ecx
+    .start:
+        cmp word [eax+ecx*2], 0
+        jnz .next
+        mov eax, ecx
+        ret
+
+; Calculates amount memory needed for converting UTF-16 string to UTF-8 string
+; In: 1) address of UTF-16 string
+;     2) number of 16-bit characters in the string
+; Out: length of resulting UTF-8 string not including
+;       extra terminating null byte
+utf16_to_utf8_length:
+    ; ESI - address of next source char
+    ; ECX - number of remaining 16-bit chars
+    ; EAX - current char
+    ; EDX - length of resulting UTF-8 string
+        push esi
+        mov esi, [esp+8]
+        mov ecx, [esp+12]
+        cld
+        xor eax, eax
+        mov edx, eax
+    .start:
+        jcxz .finish
+        lodsw
+        dec ecx
+    .start_loaded:
+
+        ; If code point is in range 00..7F then increase length by one
+        cmp eax, 0x7F
+        jbe .one
+
+        ; If code point is in range 80..7FF then increase length by two
+        cmp eax, 0x7FF
+        jbe .two
+
+        ; If code point is in range 800..D7FF or E000..FFFF
+        ; then increase length by 3.
+        cmp eax, 0xD7FF
+        jbe .three
+        cmp eax, 0xDFFF
+        ja .three
+
+        ; Range D800..DBFF is the second word of a UTF-16 surrogate pair
+        ; It is ill-formed, so we ignore it
+        cmp eax, 0xDBFF
+        jbe .start
+
+        ; Range DC00..DFFF is the first word of a UTF-16 surrogate pair,
+        ; so we read next word
+        ; If we are at the end of string we ignore ill-formed word
+        jcxz .finish
+        lodsw
+        dec ecx
+
+        ; Second word must be in range D800..DBFF,
+        ; then we increase length by 4
+        ; Otherwise we ignore previous word and start over with this word
+        cmp eax, 0xD7FF
+        jbe .start_loaded
+        cmp eax, 0xDBFF
+        ja .start_loaded
+
+        ; Correct surrogate pair - add 4 to length
+        add edx, 4
+        jmp .start
+
+    .one:
+        inc edx
+        jmp .start
+
+    .two:
+        add edx, 2
+        jmp .start
+
+    .three:
+        add edx, 3
+        jmp .start
+
+    .finish:
+        mov eax, edx
+        pop esi
+        ret
+
+; Converts UTF-16 string to UTF-8 string
+; In: 1) address of UTF-16 string
+;     2) number of 16-bit characters in the string
+;     3) address of memory for result
+;     4) length of memory for result (including terminating null byte)
+; Out: Length of UTF-8 string excluding terminating null byte
+;            If not enough memory for output returns -1.
+utf16_to_utf8:
+    ; ESI - address of next source char
+    ; EDI - address of next result char
+    ; ECX - number of remaining 16-bit chars
+    ; EAX - current char
+    ; EBX - helper for recoding
+    ; EDX - number of remaining bytes in result
+        push ebp
+        mov ebp, esp
+        push esi
+        push edi
+        push ebx
+        mov esi, [ebp+8]
+        mov ecx, [ebp+12]
+        mov edi, [ebp+16]
+        mov edx, [ebp+20]
+        cld
+        xor eax, eax
+    .start:
+        jcxz .finish
+        lodsw
+        dec ecx
+    .start_loaded:
+
+        ; If code point is in range 00..7F then output one byte
+        cmp eax, 0x7F
+        jbe .one
+
+        ; If code point is in range 80..7FF then output two bytes
+        cmp eax, 0x7FF
+        jbe .two
+
+        ; If code point is in range 800..D7FF or E000..FFFF
+        ; then ouput 3 bytes
+        cmp eax, 0xD7FF
+        jbe .three
+        cmp eax, 0xDFFF
+        ja .three
+
+        ; Range D800..DBFF is the second word of a UTF-16 surrogate pair
+        ; It is ill-formed, so we ignore it
+        cmp eax, 0xDBFF
+        jbe .start
+
+        ; Range DC00..DFFF is the first word of a UTF-16 surrogate pair,
+        ; so we read next word
+        ; If we are at the end of string we ignore ill-formed word
+        jcxz .finish
+        mov ebx, eax        ; Store first word of surrogate pair in EBX
+        lodsw
+        dec ecx
+
+        ; Second word must be in range D800..DBFF,
+        ; then output 4 bytes
+        ; Otherwise we ignore previous word and start over with this word
+        cmp eax, 0xD7FF
+        jbe .start_loaded
+        cmp eax, 0xDBFF
+        ja .start_loaded
+
+        ; Correct surrogate pair - add 4 to length
+        cmp edx, 4
+        jb .outofmemory
+        jmp .four
+
+    .finish:
+        ; Extra null-terminating byte
+        cmp edx, 1
+        jb .outofmemory
+        xor eax, eax
+        stosb
+
+        mov eax, [ebp+20]
+        sub eax, edx
+    .ret:
+        pop ebx
+        pop edi
+        pop esi
+        mov esp, ebp
+        pop ebp
+        ret
+
+    .one:
+        cmp edx, 1
+        jb .outofmemory
+        stosb
+        dec edx
+        jmp .start
+
+    .two:
+        cmp edx, 2
+        jb .outofmemory
+        mov ebx, eax
+        shr eax, 6
+        or eax, 11000000b
+        stosb
+        mov eax, ebx
+        and eax, 00111111b
+        or eax, 10000000b
+        stosb
+        sub edx, 2
+        jmp .start
+
+    .three:
+        cmp edx, 3
+        jb .outofmemory
+        mov ebx, eax
+        shr eax, 12
+        or eax, 11100000b
+        stosb
+        mov eax, ebx
+        shr eax, 6
+        and eax, 00111111b
+        or eax, 10000000b
+        mov eax, ebx
+        and eax, 00111111b
+        or eax, 10000000b
+        stosb
+        sub edx, 3
+        jmp .start
+
+    .outofmemory:
+        mov eax, -1
+        jmp .ret
+
+    .four:
+        push eax            ; Put away second word
+        push ebx            ; Put away first word
+
+        mov eax, ebx
+        shr eax, 6
+        and eax, 00001111b
+        inc eax
+        mov ebx, eax
+        shr eax, 2
+        or eax, 11110000b
+        stosb
+
+        mov eax, ebx
+        and eax, 00000011b
+        shl eax, 4
+        mov ebx, [esp]
+        shr ebx, 2
+        and ebx, 00001111b
+        or eax, ebx
+        or eax, 10000000b
+        stosb
+
+        mov eax, [esp]
+        and eax, 00000011b
+        shl eax, 4
+        mov ebx, [esp+4]
+        shr ebx, 6
+        and ebx, 00001111b
+        or eax, ebx
+        or eax, 10000000b
+        stosb
+
+        mov eax, [esp+4]
+        and eax, 00111111b
+        or eax, 10000000b
+        stosb
+
+        add esp, 8
+        sub edx, 4
+        jmp .start
+}
+
+macro PLATFORM_RDATA
+{
+section '.rdata' data readable
+}
+
+macro PLATFORM_DATA
+{
 section '.data' data readable writeable
-unicode_rwdata
-core_rwdata
-runner_rwdata
-memory_rwdata
-        
+}
+
+macro PLATFORM_BSS
+{
 section '.bss' data readable writeable
         hStdout         rd  1
         hStdin          rd  1
@@ -57,11 +498,9 @@ section '.bss' data readable writeable
         argv            rd  1
         argl            rd  1
         win32_heap      rd  1
+}
 
-unicode_bss
-core_bss
-runner_bss
-memory_bss
+include 'enscheme.inc'
 
 section '.idata' import data readable writable
 
@@ -106,7 +545,7 @@ kernel32_iat:
 shell32_iat:
         CommandLineToArgvW dd rva CommandLineToArgvW_name
         dd 0
-    
+
         kernel32_name db 'kernel32.dll', 0
         shell32_name db 'shell32.dll', 0
         align 2
