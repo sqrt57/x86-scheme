1f25ee17880ec0e1302363ea0cca1cec30a1b0b6
Dmitry Grigoryev <dmitriy57@gmail.com>
2016-07-15 02:25:45 +0300

    Implemented creating and applying lambdas, environments.

--- src/test.scm
+++ src/test.scm
@@ -1,10 +1,7 @@
-#t
-#f
-(if #f 1 0)
-(if #t 1 0)
-(if "67" 1 0)
-(if (quote 5) 1 0)
-(eq? 1 1)
-(eq? (quote a) (quote a))
-(eq? (list) (list))
+((lambda (x . y) x) 5)
+((lambda (x . y) y) 5)
+((lambda (x . y) y) 5 6 7)
+((lambda (x y z) (- (+ x y) z)) 100 20 5)
+((lambda (x) ((lambda (x) x) 5)) 8)
+((lambda (x) ((lambda (y) x) 5)) 8)
 
