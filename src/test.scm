9a91cf3ad35b8b186f6988f59ae584710d0000da
Dmitry Grigoryev <dmitriy57@gmail.com>
2016-07-20 16:34:48 +0300

    Simplified tagging in arithmetic operations

--- src/test.scm
+++ src/test.scm
@@ -1,18 +1,5 @@
-(define b 5)
-b
-(define b 6)
-b
-(set! b 7)
-b
-(define a 1)
-(define b 1)
-(define q
-  (lambda (x)
-    (define y (+ x 10))
-    (define b 100)
-    (set! a 100)
-    (+ x y)))
-(q 0)
-a
-b
+(+ 1)
+(+ 1 2)
+(- 5)
+(- 5 8)
 
