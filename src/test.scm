48135ef63760b2b61a04825be0d184e2db5e6eb5
Dmitry Grigoryev <dmitriy57@gmail.com>
2016-07-21 17:57:53 +0300

    Implemented macros

--- src/test.scm
+++ src/test.scm
@@ -1,11 +1,10 @@
-(define a 5)
-a
-(define a 8)
-a
-(set! a 9)
-a
-(define define "hello")
-define
-(set! define "world")
-define
+(if #t 5 (car 5))
+(define if1
+  (lambda (condition then else)
+    (if condition then else)))
+(define if2
+  (macro (condition then else)
+    (if (eval condition) then else)))
+(if2 #t 5 (car 5))
+(if2 #f 5 (car 5))
 
