ff36b915ca0bcfe90e64d5bcc636770154e224f5
Dmitry Grigoryev <dmitriy57@gmail.com>
2017-03-14 11:18:12 -0500

    Moved code around

--- lib/boot.scm
+++ lib/boot.scm
@@ -1,29 +1,4 @@
-(define builtin-define define)
-
-(define defmacro
-  (macro (head . body)
-    (list (quote builtin-define)
-          (car head)
-          (cons (quote macro)
-                (cons (cdr head)
-                      body)))))
-
-(defmacro (or2 x y)
-  ((lambda (x y) (if x x y)) x y))
-
-(defmacro (and2 x y)
-  ((lambda (x y) (if x y x)) x y))
-
-(defmacro (define head . body)
-  (if (pair? head)
-      (list (quote builtin-define)
-            (car head)
-            (cons (quote lambda)
-                  (cons (cdr head)
-                        body)))
-      (cons (quote builtin-define) (cons head body))))
-
-(define (string-append . strings) (reduce-left string-append-2 "" strings))
+; Top level bootstrap which sets up scheme environment
 
 (include "lib/core.scm")
 (include "lib/srfi-1.scm")
--- lib/core.scm
+++ lib/core.scm
@@ -1,3 +1,31 @@
+; Core scheme functionality
+
+(define builtin-define define)
+
+(define defmacro
+  (macro (head . body)
+    (list (quote builtin-define)
+          (car head)
+          (cons (quote macro)
+                (cons (cdr head)
+                      body)))))
+
+(defmacro (or2 x y)
+  ((lambda (x y) (if x x y)) x y))
+
+(defmacro (and2 x y)
+  ((lambda (x y) (if x y x)) x y))
+
+(defmacro (define head . body)
+  (if (pair? head)
+      (list (quote builtin-define)
+            (car head)
+            (cons (quote lambda)
+                  (cons (cdr head)
+                        body)))
+      (cons (quote builtin-define) (cons head body))))
+
+(define (string-append . strings) (reduce-left string-append-2 "" strings))
 (define (memq obj list)
   (if (pair? list)
       (if (eq? (car list) obj)
